578a9f34132133617f08fb6718ea2ab8785e79b5
Daniel Gonzalez <146906675+DANIElPEZ@users.noreply.github.com>
2025-02-06 13:10:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const $messagesui=$('ul');
 const $container=$('main');
 const $button=$('button');
 const $small=$('div');
-const SELECTED_MODEL='Llama-3.2-3B-Instruct-q4f32_1-MLC';
+const SELECTED_MODEL="Llama-3.2-1B-Instruct-q0f16-MLC";
 
 const engine=await CreateWebWorkerMLCEngine(
     new Worker('./worker.js',{type:'module'}),
